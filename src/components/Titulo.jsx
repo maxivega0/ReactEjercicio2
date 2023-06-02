7139352469489853967e187fb9c01e455776ff6b
maxivega0 <maximilianovega600@gmail.com>
2023-06-01 23:10:05 -0300

    ejercicio 2 resuelto

---
+++ src/components/Titulo.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const Titulo = ({nuevoTitulo}) => {
+    return (
+        <div>
+            <h1>Hola {nuevoTitulo}</h1>
+        </div>
+    );
+};
+
+export default Titulo;
